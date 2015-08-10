d5083306ea38fd7f217c2f2a388a93fdbb83661b
Davey <daveynewhall@gmail.com>
2015-08-09 19:53:04 -0500

    update  styles and js

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -10338,20 +10338,7 @@ if ( typeof noGlobal === strundefined ) {
 	window.jQuery = window.$ = jQuery;
 }
 
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
 
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-});
 
 
 
