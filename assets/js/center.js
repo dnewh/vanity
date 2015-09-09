6b667181566fe65b08588202ccea50e4ed06887b
Davey Newhall <newha033@umn.edu>
2015-09-09 13:58:46 -0500

    fix vert centering and scrolling

--- assets/js/center.js
+++ assets/js/center.js
@@ -8,9 +8,13 @@ $(window).resize(function()
     centerContent();
 });
 
-function centerContent()
+function centerContent(index, value)
 {
   var container = $('.container');
-  var content = $('.content');
-  content.css("left", (container.width()-content.width())/2);
+  var ids = ["#head", "#cabout", "#cresume", "#ccontact"];
+  for (var i = 0; i < ids.length; i++){
+    var v = $(ids[i])
+    v.css("left", ((container.width()-v.width())/2)+"px");
+    v.css("top", ((container.height()-v.height())/2)+"px");
+  }
 }
