cab0cc4fa282ebed3af8104608a393f8e0267fca
Davey Newhall <newha033@umn.edu>
2015-09-09 17:32:06 -0500

    update scrolling

--- assets/js/scrollTo.js
+++ assets/js/scrollTo.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
+	var divArray = ["#first", "#about", "#resume", "#contact"];
+	var index = 0;
+	var allowClick = true;
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
+			if (!allowClick)
+				return;
+			allowClick = false;
 
 	    var target = this.hash;
 	    var $target = $(target);
@@ -10,5 +16,40 @@ $(document).ready(function(){
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
+			setTimeout(function(){allowClick = true;}, 1200);
+			index = divArray.indexOf(target);
+	});
+	$(window).bind('mousewheel DOMMouseScroll', function(event){
+		if (!allowClick)
+			return;
+		if (event.originalEvent.wheelDelta > 0 && index != 0){
+			event.preventDefault();
+			allowClick = false;
+			var newInd = index - 1;
+			var target = $(divArray[newInd]);
+			var anchor = divArray[newInd];
+
+			$('html, body').stop().animate({
+					'scrollTop' : target.offset().top
+			}, 900, 'swing', function () {
+				window.location.hash = anchor;
+			});
+			setTimeout(function(){allowClick = true;}, 1200);
+			index = newInd;
+		}
+		else if (event.originalEvent.wheelDelta < 0 && index != (divArray.length)) {
+			event.preventDefault;
+			allowClick = false;
+			var newInd = index+1;
+			var target = $(divArray[newInd]);
+			var anchor = divArray[newInd];
+			$('html, body').stop().animate({
+					'scrollTop' : target.offset().top
+			}, 900, 'swing', function () {
+				window.location.hash = anchor;
+			});
+			setTimeout(function(){allowClick = true;}, 1200);
+			index = newInd;
+		}
 	});
 });
