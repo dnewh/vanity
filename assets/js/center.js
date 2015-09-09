a3e6657883270b420ca5bb9e23914c498451d61f
Davey Newhall <newha033@umn.edu>
2015-09-09 12:06:40 -0500

    change styles and pipeline

--- assets/js/center.js
+++ assets/js/center.js
@@ -13,5 +13,4 @@ function centerContent()
   var container = $('.container');
   var content = $('.content');
   content.css("left", (container.width()-content.width())/2);
-  content.css("top", (container.height()-content.height())/2);
 }
