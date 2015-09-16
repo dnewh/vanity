ac093744dc90c8be4f581ff2fecb097b02a94428
Davey Newhall <newha033@umn.edu>
2015-09-16 16:13:24 -0500

    fix nav scroll work on main nav

--- assets/js/hideShowNav.js
+++ assets/js/hideShowNav.js
@@ -1,11 +1,17 @@
-$(window).scroll({
-  scrolldown();
-},
 function scrolldown(){
   var viewportHeight = $(window).height();
-  if (this.scrollTop() > viewportHeight && $(".bmenu").left < 0){
+  if ($(window).scrollTop() > 145 && $(".bmenu").offset().left < 0){
     $(".bmenu").animate({
-      left: 0
+      left: 0 + "px"
     }, 500);
-  };
+  }
+  else if ($(window).scrollTop() < 145 && $(".bmenu").offset().left >= 0){
+    $(".bmenu").animate({
+      left: -6.2 + "em"
+    }, 500);
+  }
+}
+
+$(window).scroll(function() {
+  scrolldown();
 })
