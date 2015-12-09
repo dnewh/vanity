58c4dcfbcde771b122da84154e8483e143062752
Davey Newhall <newha033@umn.edu>
2015-12-09 15:51:52 -0600

    clean stuff up

--- assets/js/hideShowNav.js
+++ assets/js/hideShowNav.js
@@ -3,12 +3,12 @@ function scrolldown(){
   if ($(window).scrollTop() > 145 && $(".bmenu").offset().left < 0){
     $(".bmenu").animate({
       left: 0 + "px"
-    }, 500);
+    }, {queue: false, duration: 500});
   }
   else if ($(window).scrollTop() < 145 && $(".bmenu").offset().left >= 0){
     $(".bmenu").animate({
       left: -6.2 + "em"
-    }, 500);
+    }, {queue: false, duration: 500});
   }
 }
 
